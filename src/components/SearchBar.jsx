fbe208688b32c9abafc4a9f82df75ad1ca7d70c3
iamharky <harky@yandex.com>
2020-11-27 21:31:07 +0300

    Used searchContext to start searching.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,43 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
+import {SearchContext} from '../contexts/SearchContext';
 import searchIcon from '../assets/search.svg';
 import './searchbar.scss';
 
-const SearchBar = ({startSearch}) => {
+const SearchBar = () => {
   const [focused, setFocused] = useState(false);
   const [searchText, setSearchText] = useState('');
+
+  const {startSearch} = useContext(SearchContext);
 
   const setText = (e) => setSearchText(e.target.value);
 
   const runSearch = () => {
     setFocused(false);
-    searchText !== '' && startSearch(searchText);
+    searchText.trim() !== '' && startSearch(searchText);
   };
 
   const handleKeyDown = (e) => {
     if (e.code === 'Enter' || e.code === 'NumpadEnter') {
       runSearch();
-    };
-  }
+    }
+  };
 
   return (
     <div id="search-bar-wrapper">
-      <div id="search-bg" className={focused ? 'img-on-focused' : ''}/>
-      <div id="search-bar" onKeyDown={handleKeyDown} className={focused ? 'focused' : ''}>
+      <div id="search-bg" className={focused ? 'focused-bg' : ''} />
+      <div id="search-bar" onKeyDown={handleKeyDown} className={focused ? 'focused-bar' : ''}>
         <input
           type="text"
           onChange={setText}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
           value={searchText}
-          placeholder="Search for music..." />
-        <img onClick={runSearch} src={searchIcon} alt="Search icon"/>
+          placeholder="Search for music..."
+        />
+        <img onClick={runSearch} src={searchIcon} alt="Search icon" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SearchBar;
