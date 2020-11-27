c2243702d0cb6196bb859e41c298e46bc5d858ab
iamharky <harky@yandex.com>
2020-11-27 21:30:16 +0300

    Changed Context path + Prettier fixes.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,10 @@
 import React, {useContext} from 'react';
-import {Context} from '../Context';
+import {Context} from '../contexts/Context';
 import playIcon from '../assets/play.svg';
 import pauseIcon from '../assets/pause.svg';
 import './player.scss';
 
 const Player = () => {
-
   const {
     playingNow,
     audioEl,
@@ -19,17 +18,12 @@ const Player = () => {
     modifiedDuration,
     modifiedCurrentTime,
   } = useContext(Context);
-  
+
   if (playingNow) {
     return (
       <div id="player">
-        <img className="player-thumbnail" src={playingNow.album.cover_medium} alt={playingNow.title}/>
-        <img
-          className="play-pause"
-          onClick={playPause}
-          src={isPlaying ? pauseIcon : playIcon}
-          alt="Play button"
-        />
+        <img className="player-thumbnail" src={playingNow.cover} alt={playingNow.title} />
+        <img className="play-pause" onClick={playPause} src={isPlaying ? pauseIcon : playIcon} alt="Play button" />
         <audio onEnded={handleEnding} onTimeUpdate={updateProgress} ref={audioEl} src={playingNow.preview}>
           Your browser does not support the audio tag!
         </audio>
@@ -39,20 +33,17 @@ const Player = () => {
         </div>
         <span id="duration">{modifiedDuration}</span>
         <span id="playing-name">{playingNow.title}</span>
-        <a
-          href={playingNow.link}
-          id="playing-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={playingNow.link} id="playing-link" target="_blank" rel="noopener noreferrer">
           Open in Deezer
         </a>
-        <a className="playing-artist" href={'/artist/' + playingNow.artist.id}>{playingNow.artist.name}</a>
+        <a className="playing-artist" href={'/artist/' + playingNow.artist.id}>
+          {playingNow.artist.name}
+        </a>
       </div>
-    )
+    );
   } else {
     return <div></div>;
-  };
-}
+  }
+};
 
 export default Player;
